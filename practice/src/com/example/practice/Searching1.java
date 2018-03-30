834d754eaa777c3477b295c7bdb3f077dcd166db
akshigarg <akshi.garg@assaabloy.com>
2018-03-30 18:15:41 +0530

    first commit

---
+++ practice/src/com/example/practice/Searching1.java
@@ -0,0 +1,21 @@
+package com.example.practice;
+
+public class Searching1 {
+
+	public static void main(String[] args) {
+		
+		int range = 7;
+		int[] a = {1,7,1,2,1,2,3,3,3,4,2,4,5,5,5,6,6,7,6,4};
+		int xorOfInput = -1;
+		int xorOfRange = -1;
+		for(int i = 0 ; i<a.length ; i++) {
+			xorOfInput ^= a[i];
+		}
+		for(int i = 1; i <= range ; i++) {
+			xorOfRange ^= i;
+		}
+		
+		System.out.println(xorOfInput ^ xorOfRange);
+		
+	}
+}
